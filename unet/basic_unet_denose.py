8a76685b619767d40863198d2132fd8b3d281ef0
Dolly-Uppal <iitidollyuppal29@gmail.com>
2025-09-18 23:11:57 +0530

    Add files via upload

---
+++ unet/basic_unet_denose.py
@@ -0,0 +1,252 @@
+from typing import Optional, Sequence, Union
+import math 
+import torch
+import torch.nn as nn
+
+from monai.networks.blocks import Convolution, UpSample
+from monai.networks.layers.factories import Conv, Pool
+from monai.utils import deprecated_arg, ensure_tuple_rep
+
+__all__ = ["BasicUnet", "Basicunet", "basicunet", "BasicUNet"]
+
+def get_timestep_embedding(timesteps, embedding_dim):
+    
+   
+        Args:
+            spatial_dims: number of spatial dimensions.
+            in_chns: number of input channels.
+            out_chns: number of output channels.
+            act: activation type and arguments.
+            norm: feature normalization type and arguments.
+            bias: whether to have a bias term in convolution blocks.
+            dropout: dropout ratio. Defaults to no dropout.
+
+        .. deprecated:: 0.6.0
+            ``dim`` is deprecated, use ``spatial_dims`` instead.
+        """
+        super().__init__()
+        self.temb_proj = torch.nn.Linear(512,
+                                         out_chns)
+
+        if dim is not None:
+            spatial_dims = dim
+        conv_0 = Convolution(spatial_dims, in_chns, out_chns, act=act, norm=norm, dropout=dropout, bias=bias, padding=1)
+        conv_1 = Convolution(
+            spatial_dims, out_chns, out_chns, act=act, norm=norm, dropout=dropout, bias=bias, padding=1
+        )
+        self.add_module("conv_0", conv_0)
+        self.add_module("conv_1", conv_1)
+    
+    def forward(self, x, temb):
+        x = self.conv_0(x)
+        x = x + self.temb_proj(nonlinearity(temb))[:, :, None, None, None]
+        x = self.conv_1(x)
+        return x 
+
+class Down(nn.Sequential):
+    """maxpooling downsampling and two convolutions."""
+
+    @deprecated_arg(name="dim", new_name="spatial_dims", since="0.6", msg_suffix="Please use `spatial_dims` instead.")
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_chns: int,
+        out_chns: int,
+        act: Union[str, tuple],
+        norm: Union[str, tuple],
+        bias: bool,
+        dropout: Union[float, tuple] = 0.0,
+        dim: Optional[int] = None,
+    ):
+       
+        super().__init__()
+        if dim is not None:
+            spatial_dims = dim
+        max_pooling = Pool["MAX", spatial_dims](kernel_size=2)
+        convs = TwoConv(spatial_dims, in_chns, out_chns, act, norm, bias, dropout)
+        self.add_module("max_pooling", max_pooling)
+        self.add_module("convs", convs)
+
+    def forward(self, x, temb):
+        x = self.max_pooling(x)
+        x = self.convs(x, temb)
+        return x 
+
+class UpCat(nn.Module):
+    """upsampling, concatenation with the encoder feature map, two convolutions"""
+
+    @deprecated_arg(name="dim", new_name="spatial_dims", since="0.6", msg_suffix="Please use `spatial_dims` instead.")
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_chns: int,
+        cat_chns: int,
+        out_chns: int,
+        act: Union[str, tuple],
+        norm: Union[str, tuple],
+        bias: bool,
+        dropout: Union[float, tuple] = 0.0,
+        upsample: str = "deconv",
+        pre_conv: Optional[Union[nn.Module, str]] = "default",
+        interp_mode: str = "linear",
+        align_corners: Optional[bool] = True,
+        halves: bool = True,
+        dim: Optional[int] = None,
+    ):
+        """
+        Args:
+            spatial_dims: number of spatial dimensions.
+            in_chns: number of input channels to be upsampled.
+            cat_chns: number of channels from the decoder.
+            out_chns: number of output channels.
+            act: activation type and arguments.
+            norm: feature normalization type and arguments.
+            bias: whether to have a bias term in convolution blocks.
+            dropout: dropout ratio. Defaults to no dropout.
+            upsample: upsampling mode, available options are
+                ``"deconv"``, ``"pixelshuffle"``, ``"nontrainable"``.
+            pre_conv: a conv block applied before upsampling.
+                Only used in the "nontrainable" or "pixelshuffle" mode.
+            interp_mode: {``"nearest"``, ``"linear"``, ``"bilinear"``, ``"bicubic"``, ``"trilinear"``}
+                Only used in the "nontrainable" mode.
+            align_corners: set the align_corners parameter for upsample. Defaults to True.
+                Only used in the "nontrainable" mode.
+            halves: whether to halve the number of channels during upsampling.
+                This parameter does not work on ``nontrainable`` mode if ``pre_conv`` is `None`.
+
+        .. deprecated:: 0.6.0
+            ``dim`` is deprecated, use ``spatial_dims`` instead.
+        """
+        super().__init__()
+        if dim is not None:
+            spatial_dims = dim
+        if upsample == "nontrainable" and pre_conv is None:
+            up_chns = in_chns
+        else:
+            up_chns = in_chns // 2 if halves else in_chns
+        self.upsample = UpSample(
+            spatial_dims,
+            in_chns,
+            up_chns,
+            2,
+            mode=upsample,
+            pre_conv=pre_conv,
+            interp_mode=interp_mode,
+            align_corners=align_corners,
+        )
+        self.convs = TwoConv(spatial_dims, cat_chns + up_chns, out_chns, act, norm, bias, dropout)
+
+    def forward(self, x: torch.Tensor, x_e: Optional[torch.Tensor], temb):
+        """
+
+        Args:
+            x: features to be upsampled.
+            x_e: features from the encoder.
+        """
+        x_0 = self.upsample(x)
+
+        if x_e is not None:
+            # handling spatial shapes due to the 2x maxpooling with odd edge lengths.
+            dimensions = len(x.shape) - 2
+            sp = [0] * (dimensions * 2)
+            for i in range(dimensions):
+                if x_e.shape[-i - 1] != x_0.shape[-i - 1]:
+                    sp[i * 2 + 1] = 1
+            x_0 = torch.nn.functional.pad(x_0, sp, "replicate")
+            x = self.convs(torch.cat([x_e, x_0], dim=1), temb)  # input channels: (cat_chns + up_chns)
+        else:
+            x = self.convs(x_0, temb)
+
+        return x
+
+
+class BasicUNetDe(nn.Module):
+    @deprecated_arg(
+        name="dimensions", new_name="spatial_dims", since="0.6", msg_suffix="Please use `spatial_dims` instead."
+    )
+    def __init__(
+        self,
+        spatial_dims: int = 3,
+        in_channels: int = 1,
+        out_channels: int = 2,
+        features: Sequence[int] = (32, 32, 64, 128, 256, 32),
+        act: Union[str, tuple] = ("LeakyReLU", {"negative_slope": 0.1, "inplace": True}),
+        norm: Union[str, tuple] = ("instance", {"affine": True}),
+        bias: bool = True,
+        dropout: Union[float, tuple] = 0.0,
+        upsample: str = "deconv",
+        dimensions: Optional[int] = None,
+    ):
+        """
+        
+
+        """
+        super().__init__()
+        if dimensions is not None:
+            spatial_dims = dimensions
+
+        fea = ensure_tuple_rep(features, 6)
+        print(f"BasicUNet features: {fea}.")
+        
+        # timestep embedding
+        self.temb = nn.Module()
+        self.temb.dense = nn.ModuleList([
+            torch.nn.Linear(128,
+                            512),
+            torch.nn.Linear(512,
+                            512),
+        ])
+
+        self.conv_0 = TwoConv(spatial_dims, in_channels, features[0], act, norm, bias, dropout)
+        self.down_1 = Down(spatial_dims, fea[0], fea[1], act, norm, bias, dropout)
+        self.down_2 = Down(spatial_dims, fea[1], fea[2], act, norm, bias, dropout)
+        self.down_3 = Down(spatial_dims, fea[2], fea[3], act, norm, bias, dropout)
+        self.down_4 = Down(spatial_dims, fea[3], fea[4], act, norm, bias, dropout)
+
+        self.upcat_4 = UpCat(spatial_dims, fea[4], fea[3], fea[3], act, norm, bias, dropout, upsample)
+        self.upcat_3 = UpCat(spatial_dims, fea[3], fea[2], fea[2], act, norm, bias, dropout, upsample)
+        self.upcat_2 = UpCat(spatial_dims, fea[2], fea[1], fea[1], act, norm, bias, dropout, upsample)
+        self.upcat_1 = UpCat(spatial_dims, fea[1], fea[0], fea[5], act, norm, bias, dropout, upsample, halves=False)
+
+        self.final_conv = Conv["conv", spatial_dims](fea[5], out_channels, kernel_size=1)
+
+    def forward(self, x: torch.Tensor, t, embeddings=None, image=None):
+        
+        temb = get_timestep_embedding(t, 128)
+        temb = self.temb.dense[0](temb)
+        temb = nonlinearity(temb)
+        temb = self.temb.dense[1](temb)
+
+        if image is not None :
+            x = torch.cat([image, x], dim=1)
+            
+        x0 = self.conv_0(x, temb)
+        if embeddings is not None:
+            x0 += embeddings[0]
+
+        x1 = self.down_1(x0, temb)
+        if embeddings is not None:
+            x1 += embeddings[1]
+
+        x2 = self.down_2(x1, temb)
+        if embeddings is not None:
+            x2 += embeddings[2]
+
+        x3 = self.down_3(x2, temb)
+        if embeddings is not None:
+            x3 += embeddings[3]
+
+        x4 = self.down_4(x3, temb)
+        if embeddings is not None:
+            x4 += embeddings[4]
+
+        u4 = self.upcat_4(x4, x3, temb)
+        u3 = self.upcat_3(u4, x2, temb)
+        u2 = self.upcat_2(u3, x1, temb)
+        u1 = self.upcat_1(u2, x0, temb)
+
+        logits = self.final_conv(u1)
+        return logits
+
+
+
